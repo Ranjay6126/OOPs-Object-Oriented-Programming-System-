e50cee2d73b8a0e758ef1717b74f227218de66ad
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-23 21:12:05 +0530

    single inheritance

---
+++ Single_Inheritance.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class PersonA{
+public:
+    string name;
+    int age;
+};
+
+class PersonB : public PersonA{
+public:
+    int rollno;
+
+    void getInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Roll No: " << rollno << endl;
+    }
+};
+
+int main(){
+
+    PersonB b;
+
+    // Assign values manually
+    b.name = "Alice";
+    b.age = 20;
+    b.rollno = 123;
+    b.getInfo();
+    return 0;
+}
