26513b020f838c44a1984c4c41f5a9eb5106dc21
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-23 20:39:24 +0530

    add parent and child output

--- inheritances.cpp
+++ inheritances.cpp
@@ -1,3 +1,5 @@
+/* Inheritance in C++ allows a new class (derived class) to inherit properties and behaviors (data members and member functions) from an existing class (base class). This promotes code reusability and establishes a natural hierarchical relationship between classes.
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -30,5 +32,47 @@ int main(){
 
     b.getInfo();
 
+    return 0;
+} */
+
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class PersonA{
+public:
+    string name;
+    int age;
+
+    // Base class constructor
+    PersonA(string n, int a){
+        cout << "I am parent constructor" << endl;
+        name = n;
+        age = a;
+    }
+};
+
+class PersonB : public PersonA{
+public:
+    int rollno;
+
+    // Derived class constructor
+    PersonB(string n, int a, int r) : PersonA(n, a){
+        cout << "I am child constructor" << endl;
+        rollno = r;
+    }
+
+    void getInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Roll No: " << rollno << endl;
+    }
+};
+
+int main(){
+    PersonB b("Alice", 20, 123);
+    b.getInfo();
     return 0;
 }
