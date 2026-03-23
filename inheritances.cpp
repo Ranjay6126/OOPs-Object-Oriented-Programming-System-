8be8179ea1453477337d99a11428ad75032b059b
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-03-23 20:14:15 +0530

    inheritance

---
+++ inheritances.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class PersonA{
+public:
+    string name;
+    int age;
+
+    // Base class constructor
+    PersonA(string n, int a){
+        name = n;
+        age = a;
+    }
+};
+
+class PersonB : public PersonA{
+public:
+    int rollno;
+
+    // Derived class constructor
+    PersonB(string n, int a, int r) : PersonA(n, a){
+        rollno = r;
+    }
+
+    void getInfo(){
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+        cout << "Roll No: " << rollno << endl;
+    }
+};
+
+int main(){
+
+    PersonB b("Alice", 20, 123);
+
+    b.getInfo();
+
+    return 0;
+}
