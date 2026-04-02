bb28980a4eaee2960800be401c8942f4338447fa
Ranjay6126 <6126ranjayprajapati@gmail.com>
2026-04-02 23:12:26 +0530

    add encap

--- Encapsulation.cpp
+++ Encapsulation.cpp
@@ -1,36 +1,59 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Account{
-
-    //data hiding by using private access specifier
-    private:
-
+class Account {
+private:
     double balance;
     string password;
 
-    public:
-
+public:
     string accountId;
-    string username;    
+    string username;
 
+    // Constructor
+    Account() : balance(0.0), password("") {}
+
+    // Setters provide controlled access (write)
+    void setBalance(double amount) {
+        if (amount >= 0.0) {
+            balance = amount;
+        }
+    }
+
+    void setPassword(const string& pwd) {
+        if (!pwd.empty()) {
+            password = pwd;
+        }
+    }
+
+    // Getters provide controlled access (read)
+    double getBalance() const {
+        return balance;
+    }
+
+    bool checkPassword(const string& pwd) const {
+        return password == pwd;
+    }
 };
 
-int main(){
-
+int main() {
     Account acc;
     acc.accountId = "ACC001";
     acc.username = "john_doe";
 
-    acc.balance = 10000.0; // Error: 'balance' is a private member of 'Account'
+    acc.setBalance(10000.0);
+    acc.setPassword("secure123");
 
-    cout<< acc.balance <<endl;// Error: 'balance' is a private member of 'Account'
+    cout << "Account ID: " << acc.accountId << endl;
+    cout << "Username: " << acc.username << endl;
+    cout << "Balance: " << acc.getBalance() << endl;
 
-
-    cout<< acc.accountId <<endl;
-    cout<< acc.username<<endl;
+    if (acc.checkPassword("secure123")) {
+        cout << "Password verified" << endl;
+    } else {
+        cout << "Password invalid" << endl;
+    }
 
     return 0;
-
 }
